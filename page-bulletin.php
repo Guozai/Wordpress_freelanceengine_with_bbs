3052a62b67c22382fa38b7e97ce3c639d5ee660a
Yiping Guo <yiping_guo@outlook.com>
2021-01-29 15:27:39 +0800

    Finish page-bulletin, start bulletin list

--- page-bulletin.php
+++ page-bulletin.php
@@ -11,10 +11,17 @@
  * @subpackage FreelanceEngine
  * @since FreelanceEngine 1.8.2
  */
-global $user_ID;
+global $ae_post_factory, $user_ID;
 get_header();
-
+if ( isset($_REQUEST['id']) ) {
+    $post = get_post($_REQUEST['id']);
+    if ( $post ) {
+        $post_object = $ae_post_factory->get($post->post_type);
+        $post_convert = $post_object->convert($post);
+    }
+}
 ?>
+
 <div class="fre-page-wrapper">
     <div class="fre-page-title">
         <div clsss="container">
@@ -24,10 +31,19 @@ get_header();
 
     <div class="fre-page-section">
         <div class="container">
-            <div class="page-post-bulletin-wrap" id="post-place">
-                <?php
-                    
-                ?>
+            <div class="bulletin-wrap">
+                <div class="fre-bulletin-box">
+                    <div class="bulletin-freelance-info-wrap active">
+                        <?php //if ( !empty($bulletin) ) { ?>
+                            <!-- Box show bulletin post detail-->
+                            <div class="freelance-bulletin-wrap">
+                                <h2><?php //echo $bulletin->title ?></h2>
+                                <?php //echo apply_filters( 'the_content', $bulletin->content ) ?>
+                            </div>
+                            <!-- End Box show bulletin post detail-->
+                        <?php //} ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
